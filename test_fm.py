58831665f081c8085a3f13f869fad1078359bbdb
CoolGuy75562 <82740690+CoolGuy75562@users.noreply.github.com>
2024-10-23 13:52:16 +1300

    fixed test_fm

--- test_fm.py
+++ test_fm.py
@@ -36,19 +36,19 @@ class TestReshapeList(unittest.TestCase):
     def test_different_chain_lengths(self):
         vals = [1, 2, 3, 4, 5, 6]
         algorithm = [3, 1, 2]
-        expected = [[1, 2, 3], 4, [5, 6]]
+        expected = [[1, 2, 3], [4], [5, 6]]
         self.assertEqual(fm.reshape_list(vals, algorithm), expected)
         
     def test_single_chains(self):
         vals = [1, 2, 3, 4, 5, 6]
         algorithm = [1, 1, 1, 1, 1, 1]
-        expected = [1, 2, 3, 4, 5, 6]
+        expected = [[1], [2], [3], [4], [5], [6]]
         self.assertEqual(fm.reshape_list(vals, algorithm), expected)
         
     def test_small_algorithms(self):
         vals = [1, 2]
         algorithm = [1, 1]
-        expected = [1, 2]
+        expected = [[1], [2]]
         self.assertEqual(fm.reshape_list(vals, algorithm), expected)
         
         algorithm = [2]
@@ -57,7 +57,7 @@ class TestReshapeList(unittest.TestCase):
         
         vals = [1]
         algorithm = [1]
-        expected = [1]
+        expected = [[1]]
         self.assertEqual(fm.reshape_list(vals, algorithm), expected)
         
     def test_incompatible_args_raises_error(self):
@@ -73,11 +73,11 @@ class TestReshapeList(unittest.TestCase):
 class TestNewPatchAlgorithm(unittest.TestCase):
 
     def test_equal_chain_lengths(self):
-        output_env = 1
+        output_env = []
         alg = [2, 2, 2]
-        freqs = [[1, 1], [1, 1], [1, 1]]
+        freqs = [[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]
         mod_indices = freqs
-        envs = freqs
+        envs = [[[], []], [[], []], [[], []]]
         feedback = [[0,0], [0,0], [0,0]]
         mod_0 = [0, 0, 0]
         expected_patch = {"freqs" : freqs,
@@ -91,12 +91,12 @@ class TestNewPatchAlgorithm(unittest.TestCase):
         self.assertEqual(fm.new_patch_algorithm(alg), expected_patch)
 
     def test_different_chain_lengths(self):
-        output_env = 1
+        output_env = []
         alg = [1, 2, 3]
-        freqs = [1, [1, 1], [1, 1, 1]]
+        freqs = [[1.0], [1.0, 1.0], [1.0, 1.0, 1.0]]
         mod_indices = freqs
-        envs = freqs
-        feedback = [0, [0,0], [0,0,0]]
+        envs = [[[]], [[], []], [[], [], []]]
+        feedback = [[0], [0,0], [0,0,0]]
         mod_0 = [0, 0, 0]
         expected_patch = {"freqs" : freqs,
                           "mod_indices" : mod_indices,
@@ -109,12 +109,12 @@ class TestNewPatchAlgorithm(unittest.TestCase):
         self.assertEqual(fm.new_patch_algorithm(alg), expected_patch)
 
     def test_single_chains(self):
-        output_env = 1
+        output_env = []
         alg = [1, 1, 1]
-        freqs = [1, 1, 1]
+        freqs = [[1.0], [1.0], [1.0]]
         mod_indices = freqs
-        envs = freqs
-        feedback = [0, 0, 0]
+        envs = [[[]], [[]], [[]]]
+        feedback = [[0], [0], [0]]
         mod_0 = [0, 0, 0]
         expected_patch = {"freqs" : freqs,
                  "mod_indices" : mod_indices,
@@ -127,12 +127,12 @@ class TestNewPatchAlgorithm(unittest.TestCase):
         self.assertEqual(fm.new_patch_algorithm(alg), expected_patch)
 
     def test_small_algorithms(self):
-        output_env = 1
+        output_env = []
         alg = [1]
-        freqs = [1]
+        freqs = [[1.0]]
         mod_indices = freqs
-        envs = freqs
-        feedback = [0]
+        envs = [[[]]]
+        feedback = [[0]]
         mod_0 = [0]
         expected_patch = {"freqs" : freqs,
                           "mod_indices" : mod_indices,
@@ -145,9 +145,9 @@ class TestNewPatchAlgorithm(unittest.TestCase):
         self.assertEqual(fm.new_patch_algorithm(alg), expected_patch)
 
         alg = [2]
-        freqs = [[1,1]]
+        freqs = [[1.0,1.0]]
         mod_indices = freqs
-        envs = freqs
+        envs = [[[], []]]
         feedback = [[0,0]]
         mod_0 = [0]
         expected_patch = {"freqs" : freqs,
@@ -159,33 +159,6 @@ class TestNewPatchAlgorithm(unittest.TestCase):
                  "feedback" : feedback
                  }
         self.assertEqual(fm.new_patch_algorithm(alg), expected_patch)
-
-class TestSynth(unittest.TestCase):
-    
-    def setUp(self):
-        self.default_patch = {"freqs" : [[14, 1], [1, 1], [1, 1]],
-                 "mod_indices" : [[0, 58/99], [0, 89/99], [0,79/99]],
-                 "envs" : [[1, 1], [1, 1], [1, 1]],
-                 "output_env" : [0.0125, 0.025, 0.15, 0.7, 0.05],
-                 "mod_0" : [0, 0, 0],
-                 "algorithm" : [2, 2, 2],
-                 "feedback" : [[0, 0], [0, 0], [0, 0]]
-                 }
-        self.synth = fm.Synth(self.default_patch)
-
-    def test_num_synth_outputs(self):
-        self.assertEqual(len(getattr(self.synth, 'chain_outputs')), 3)
-
-    def test_get_patch_param(self):
-        param_names = ["freqs", "mod_indices", "envs", "mod_0", "algorithm", "feedback"]
-        expecteds = [[14, 1, 1, 1, 1, 1],
-                    [0, 58/99, 0, 89/99, 0, 79/99],
-                    [1, 1, 1, 1, 1, 1],
-                    [0, 0, 0],
-                    [2, 2, 2],
-                    [0, 0, 0, 0, 0, 0]]
-        for param_name, expected in zip(param_names, expecteds):
-            self.assertEqual(self.synth.get_patch_param(param_name), expected)
 
 if __name__ == '__main__':
     unittest.main()
