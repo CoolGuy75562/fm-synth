20bb5eeb8d3405d2e6dfe86418d4c084ab03745c
CoolGuy75562 <82740690+CoolGuy75562@users.noreply.github.com>
2024-10-28 09:41:43 +1300

    licence

--- gui.py
+++ gui.py
@@ -1,6 +1,18 @@
 """ This module contains the main method and the Gtk frontend for the
 FM synthesizer.
 """
+# Copyright (C) 2024  CoolGuy75562
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
 import fm # ./fm.py
 
 import gi
--- test_fm.py
+++ test_fm.py
@@ -1,3 +1,16 @@
+""" Tests for the fm module. """
+# Copyright (C) 2024  CoolGuy75562
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
 import unittest
 import fm
 import numpy as np
